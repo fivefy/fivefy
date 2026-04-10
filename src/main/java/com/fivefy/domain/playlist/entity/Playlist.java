71852bd8577d7b4d80bb22316f9a3a3f563abd76
jihyeon <yio1346@gmail.com>
2026-04-10 20:01:29 +0900

    feat: Playlist 정적 팩토리 메서드에 유효성 검증 추가

--- src/main/java/com/fivefy/domain/playlist/entity/Playlist.java
+++ src/main/java/com/fivefy/domain/playlist/entity/Playlist.java
@@ -8,6 +8,7 @@ import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.LastModifiedDate;
 
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 @Getter
 @Entity
@@ -33,11 +34,28 @@ public class Playlist extends BaseEntity {
     private LocalDateTime deletedAt;
 
     public static Playlist create(Long userId, String title, String description) {
+        validateNonNull(userId, title);
+        validate(title);
+
         Playlist playlist = new Playlist();
         playlist.userId = userId;
         playlist.title = title;
         playlist.description = description;
 
         return playlist;
+    }
+
+    private static void validateNonNull(Long userId, String title) {
+        Objects.requireNonNull(userId, "userId는 필수 입니다");
+        Objects.requireNonNull(title, "title은 필수 입니다");
+    }
+
+    private static void validate(String title) {
+        if (title.isBlank()) {
+            throw new IllegalArgumentException("title은 공백일 수 없습니다");
+        }
+        if (title.length() > 100){
+            throw new IllegalArgumentException("title 길이는 100자 이하 입니다");
+        }
     }
 }
