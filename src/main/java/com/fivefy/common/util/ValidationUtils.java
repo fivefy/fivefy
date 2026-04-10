5d974afc28aabd505e96c93f075f7eb291dc56aa
HYOKYUNG BANG <101386329+Banhklo2@users.noreply.github.com>
2026-04-10 20:22:30 +0900

    refactor: null 검증 수정

--- src/main/java/com/fivefy/common/util/ValidationUtils.java
+++ src/main/java/com/fivefy/common/util/ValidationUtils.java
@@ -1,10 +1,10 @@
 package com.fivefy.common.util;
 
+import java.util.Objects;
+
 public class ValidationUtils {
 
-    public static void validateNonNull(Object value, String fieldName) {
-        if (value == null) {
-            throw new IllegalArgumentException(fieldName + "은/는 필수입니다.");
-        }
+    private static void validateNotNull(Object value, String fieldName) {
+        Objects.requireNonNull(value, fieldName + "는 필수입니다. (validationNotNull)");
     }
 }
