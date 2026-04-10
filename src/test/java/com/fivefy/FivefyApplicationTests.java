dd5e359248ce2edad116a1654a401276ab6f4ca8
곽현민 <hunmin111@gmail.com>
2026-04-10 14:25:39 +0900

    init: 초기 환경 세팅

---
+++ src/test/java/com/fivefy/FivefyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fivefy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FivefyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
