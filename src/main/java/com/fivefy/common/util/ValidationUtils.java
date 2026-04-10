a6d8eda115756d3a0626900fc20f9e8b46712708
HYOKYUNG BANG <101386329+Banhklo2@users.noreply.github.com>
2026-04-10 20:18:14 +0900

    fix: 오타 수정

--- src/main/java/com/fivefy/common/util/ValidationUtils.java
+++ src/main/java/com/fivefy/common/util/ValidationUtils.java
@@ -2,7 +2,7 @@ package com.fivefy.common.util;
 
 public class ValidationUtils {
 
-    public static void validateNotNull(Object value, String fieldName) {
+    public static void validateNonNull(Object value, String fieldName) {
         if (value == null) {
             throw new IllegalArgumentException(fieldName + "은/는 필수입니다.");
         }
