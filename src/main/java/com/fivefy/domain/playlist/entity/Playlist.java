132ba523e3ed924a37fdef8c7856a0104567a3ce
jihyeon <yio1346@gmail.com>
2026-04-10 15:37:21 +0900

    feat: playlist entity 생성

---
+++ src/main/java/com/fivefy/domain/playlist/entity/Playlist.java
@@ -0,0 +1,44 @@
+package com.fivefy.domain.playlist.entity;
+
+import com.fivefy.common.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Entity
+@Table(name = "playlists")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Playlist extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Long userId;
+
+    @Column(nullable = false, length = 100)
+    private String title;
+
+    @Column(nullable = false)
+    private String description;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+
+    private LocalDateTime deletedAt;
+
+    public static Playlist create(Long userId, String title, String description) {
+        Playlist playlist = new Playlist();
+        playlist.userId = userId;
+        playlist.title = title;
+        playlist.description = description;
+
+        return playlist;
+    }
+}
