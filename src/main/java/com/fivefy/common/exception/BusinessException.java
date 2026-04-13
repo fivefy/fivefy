9ca497ec6ed6f9c125f860fad871016ec661b74c
곽현민 <hunmin111@gmail.com>
2026-04-13 12:14:16 +0900

    feat: BusinessException 및 공통 에러 구조 설계
    - 도메인별 에러 메시지와 상태 코드를 정의할 ErrorCode 인터페이스 추가
    - 서비스 로직 내 예외 처리를 전담할 BusinessException 구현
    - GlobalExceptionHandler에 BusinessException 처리 핸들러 추가
    - 발생한 비즈니스 예외의 상태 코드와 메시지를 BaseResponse 규격에 맞춰 응답하도록 구성

---
+++ src/main/java/com/fivefy/common/exception/BusinessException.java
@@ -0,0 +1,15 @@
+package com.fivefy.common.exception;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public class BusinessException extends RuntimeException {
+
+    private final HttpStatus httpStatus;
+
+    public BusinessException(ErrorCode errorCode) {
+        super(errorCode.getMessage());
+        this.httpStatus = errorCode.getHttpStatus();
+    }
+}
