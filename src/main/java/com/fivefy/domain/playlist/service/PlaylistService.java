e5893d359fbf9ba9d3d4b5a5b4f094f635ac368d
HYOKYUNG BANG <101386329+Banhklo2@users.noreply.github.com>
2026-04-13 14:12:17 +0900

    feat: 플레이리스트 생성 기능 service 구현

--- src/main/java/com/fivefy/domain/playlist/service/PlaylistService.java
+++ src/main/java/com/fivefy/domain/playlist/service/PlaylistService.java
@@ -2,6 +2,7 @@ package com.fivefy.domain.playlist.service;
 
 import com.fivefy.common.dto.response.PageResponse;
 import com.fivefy.common.exception.BusinessException;
+import com.fivefy.domain.playlist.dto.request.PlaylistCreateRequest;
 import com.fivefy.domain.playlist.dto.response.PlaylistResponse;
 import com.fivefy.domain.playlist.entity.Playlist;
 import com.fivefy.domain.playlist.enums.PlaylistErrorCode;
@@ -14,12 +15,24 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
-@Transactional
+@Transactional(readOnly = true)
 public class PlaylistService {
 
     private final PlaylistRepository playlistRepository;
 
-    @Transactional(readOnly = true)
+    @Transactional
+    public PlaylistResponse createPlaylist(Long userId, PlaylistCreateRequest request) {
+        Playlist playlist = Playlist.create(
+                userId,
+                request.title(),
+                request.description()
+        );
+
+        Playlist savedPlaylist = playlistRepository.save(playlist);
+
+        return PlaylistResponse.from(savedPlaylist);
+    }
+
     public PageResponse<PlaylistResponse> getPlaylists(Pageable pageable) {
         Page<PlaylistResponse> page = playlistRepository.findAllByDeletedAtIsNull(pageable)
                 .map(PlaylistResponse::from);
@@ -27,7 +40,6 @@ public class PlaylistService {
         return PageResponse.from(page);
     }
 
-    @Transactional(readOnly = true)
     public PlaylistResponse getPlaylist(Long playlistId) {
         Playlist playlist = playlistRepository.findByIdAndDeletedAtIsNull(playlistId)
                 .orElseThrow(() -> new BusinessException(PlaylistErrorCode.PLAYLIST_NOT_FOUND));
