cad404b75d9b8259d0425bf42ff62fc58095f5bb
HYOKYUNG BANG <101386329+Banhklo2@users.noreply.github.com>
2026-04-14 10:49:44 +0900

    feat: PlaylistTrack 에러 코드 추가

--- src/main/java/com/fivefy/domain/playlist/enums/PlaylistErrorCode.java
+++ src/main/java/com/fivefy/domain/playlist/enums/PlaylistErrorCode.java
@@ -9,11 +9,17 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum PlaylistErrorCode implements ErrorCode {
 
+    // Playlist
     PLAYLIST_NOT_FOUND(HttpStatus.NOT_FOUND, "플레이리스트를 찾을 수 없습니다"),
     INVALID_TITLE(HttpStatus.BAD_REQUEST, "플레이리스트 제목은 필수입니다"),
     DUPLICATE_PLAYLIST_NAME(HttpStatus.CONFLICT, "이미 동일한 이름의 플레이리스트가 존재합니다"),
     PLAYLIST_UPDATE_FORBIDDEN(HttpStatus.FORBIDDEN, "플레이리스트 수정 권한이 없습니다"),
-    PLAYLIST_DELETE_FORBIDDEN(HttpStatus.FORBIDDEN, "플레이리스트 삭제 권한이 없습니다");
+    PLAYLIST_DELETE_FORBIDDEN(HttpStatus.FORBIDDEN, "플레이리스트 삭제 권한이 없습니다"),
+
+    // PlaylistTrack
+    PLAYLIST_TRACK_NOT_FOUND(HttpStatus.NOT_FOUND, "플레이리스트 트랙을 찾을 수 없습니다."),
+    PLAYLIST_TRACK_ALREADY_EXISTS(HttpStatus.CONFLICT, "이미 플레이리스트에 추가된 트랙입니다."),
+    INVALID_PLAYLIST_TRACK_POSITION(HttpStatus.BAD_REQUEST, "유효하지 않은 트랙 순서입니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
