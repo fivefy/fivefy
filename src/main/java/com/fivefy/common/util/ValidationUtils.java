367faaa43dfd1d9c55c9d475e93d463e090ada85
perfect-Bee <joonslee3@gmail.com>
2026-04-14 21:23:43 +0900

    feat: ValidationUtils에 validatePositive 추가

--- src/main/java/com/fivefy/common/util/ValidationUtils.java
+++ src/main/java/com/fivefy/common/util/ValidationUtils.java
@@ -7,4 +7,11 @@ public class ValidationUtils {
     public static void validateNonNull(Object value, String fieldName) {
         Objects.requireNonNull(value, fieldName + "(은)는 필수입니다");
     }
+
+    public static void validatePositive(Long value, String fieldName) {
+        validateNonNull(value, fieldName);
+        if (value <= 0) {
+            throw new NullPointerException(fieldName + "(은)는 0보다 커야 합니다");
+        }
+    }
 }
