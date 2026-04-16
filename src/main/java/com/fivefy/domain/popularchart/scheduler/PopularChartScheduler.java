a5c047d7f6b202cedae7030b4c772b9e81fbadb7
HYOKYUNG BANG <101386329+Banhklo2@users.noreply.github.com>
2026-04-16 16:56:30 +0900

    feat: 인기 차트 주간 자동 생성을 위한 스케줄러 추가

---
+++ src/main/java/com/fivefy/domain/popularchart/scheduler/PopularChartScheduler.java
@@ -0,0 +1,21 @@
+package com.fivefy.domain.popularchart.scheduler;
+
+import com.fivefy.domain.popularchart.service.PopularChartGenerateService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+
+@Component
+@RequiredArgsConstructor
+public class PopularChartScheduler {
+
+    private final PopularChartGenerateService popularChartGenerateService;
+
+    @Scheduled(cron = "0 0 0 * * MON")
+    public void generateWeeklyChart() {
+        LocalDate today = LocalDate.now();
+        popularChartGenerateService.generateWeeklyChart(today);
+    }
+}
