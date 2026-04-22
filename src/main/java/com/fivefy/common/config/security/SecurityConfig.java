0cba8980bf06a7a17153281e4a7b4839e073b562
곽현민 <hunmin111@gmail.com>
2026-04-22 17:08:56 +0900

    feat: 헬스체크 설정 추가

--- src/main/java/com/fivefy/common/config/security/SecurityConfig.java
+++ src/main/java/com/fivefy/common/config/security/SecurityConfig.java
@@ -40,6 +40,7 @@ public class SecurityConfig {
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
+                                "/acuator/health",
                                 "/api/users/signup",
                                 "/api/users/login",
                                 "/api/users/reissue",
