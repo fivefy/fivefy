cc457d555a64169d5b996db593578b3fb692f3a8
HYOKYUNG BANG <101386329+Banhklo2@users.noreply.github.com>
2026-04-22 14:37:04 +0900

    fix: playlist cleanup scheduler에 ShedLock 적용

--- src/main/java/com/fivefy/domain/playlist/scheduler/PlaylistCleanupScheduler.java
+++ src/main/java/com/fivefy/domain/playlist/scheduler/PlaylistCleanupScheduler.java
@@ -2,6 +2,7 @@ package com.fivefy.domain.playlist.scheduler;
 
 import com.fivefy.domain.playlist.service.PlaylistCleanupService;
 import lombok.RequiredArgsConstructor;
+import net.javacrumbs.shedlock.spring.annotation.SchedulerLock;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -14,6 +15,7 @@ public class PlaylistCleanupScheduler {
     private final PlaylistCleanupService playlistCleanupService;
 
     @Scheduled(cron = "0 0 3 * * *")
+    @SchedulerLock(name = "playlistCleanupScheduler_cleanup", lockAtMostFor = "10m", lockAtLeastFor = "1m")
     public void cleanup() {
         LocalDateTime threshold = LocalDateTime.now().minusDays(30);
         playlistCleanupService.cleanupDeletedPlaylists(threshold);
