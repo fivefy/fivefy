dc286e4eea9756df0ee2d114f8df9ecd24e0d426
popo2381 <popo2381@icloud.com>
2026-04-28 12:15:03 +0900

    test: integrated seed FK 검증 쿼리 통합
    - validate.sql의 FK 검증 섹션을 FK CHECK로 통합
    - 지갑/포인트/결제/구독 FK 유효성 검증 추가
    - 음악 콘텐츠/행동 로그/플레이리스트 FK 검증 추가
    - 신청 도메인 requester / reviewer 참조 검증 추가
    - 중복된 행동 로그 전용 FK 검증 섹션 제거

--- scripts/seed/datasets/integrated/validate.sql
+++ scripts/seed/datasets/integrated/validate.sql
@@ -16,63 +16,41 @@ UNION ALL SELECT 'artist_applications', COUNT(*) FROM artist_applications
 UNION ALL SELECT 'album_applications', COUNT(*) FROM album_applications
 UNION ALL SELECT 'track_applications', COUNT(*) FROM track_applications;
 
-SELECT 'playlist_tracks_invalid_playlist' AS check_name, COUNT(*) AS invalid_count
-FROM playlist_tracks pt
-LEFT JOIN playlists p ON pt.playlist_id = p.id
-WHERE p.id IS NULL;
 
-SELECT 'playlist_tracks_invalid_track' AS check_name, COUNT(*) AS invalid_count
-FROM playlist_tracks pt
-LEFT JOIN tracks t ON pt.track_id = t.id
-WHERE t.id IS NULL;
+SELECT '==================== FK CHECK ====================' AS section;
+SELECT 'wallets_invalid_user' AS check_name, COUNT(*) AS invalid_count FROM wallets w LEFT JOIN users u ON w.user_id = u.id WHERE u.id IS NULL
+UNION ALL SELECT 'point_histories_invalid_wallet', COUNT(*) FROM point_histories ph LEFT JOIN wallets w ON ph.wallet_id = w.id WHERE w.id IS NULL
+UNION ALL SELECT 'billing_keys_invalid_user', COUNT(*) FROM billing_keys bk LEFT JOIN users u ON bk.user_id = u.id WHERE u.id IS NULL
+UNION ALL SELECT 'cash_orders_invalid_user', COUNT(*) FROM cash_orders co LEFT JOIN users u ON co.user_id = u.id WHERE u.id IS NULL
+UNION ALL SELECT 'point_orders_invalid_user', COUNT(*) FROM point_orders po LEFT JOIN users u ON po.user_id = u.id WHERE u.id IS NULL
+UNION ALL SELECT 'payments_invalid_user', COUNT(*) FROM payments p LEFT JOIN users u ON p.user_id = u.id WHERE u.id IS NULL
+UNION ALL SELECT 'subscriptions_invalid_user', COUNT(*) FROM subscriptions s LEFT JOIN users u ON s.user_id = u.id WHERE u.id IS NULL
+UNION ALL SELECT 'subscriptions_invalid_point_order', COUNT(*) FROM subscriptions s LEFT JOIN point_orders po ON s.point_order_id = po.id WHERE po.id IS NULL
+UNION ALL SELECT 'artists_invalid_owner_user', COUNT(*) FROM artists a LEFT JOIN users u ON a.owner_user_id = u.id WHERE u.id IS NULL
+UNION ALL SELECT 'albums_invalid_artist', COUNT(*) FROM albums a LEFT JOIN artists ar ON a.artist_id = ar.id WHERE ar.id IS NULL
+UNION ALL SELECT 'tracks_invalid_owner_user', COUNT(*) FROM tracks t LEFT JOIN users u ON t.owner_user_id = u.id WHERE u.id IS NULL
+UNION ALL SELECT 'tracks_invalid_artist', COUNT(*) FROM tracks t LEFT JOIN artists a ON t.artist_id = a.id WHERE t.artist_id IS NOT NULL AND a.id IS NULL
+UNION ALL SELECT 'tracks_invalid_album', COUNT(*) FROM tracks t LEFT JOIN albums a ON t.album_id = a.id WHERE t.album_id IS NOT NULL AND a.id IS NULL
+UNION ALL SELECT 'likes_invalid_user', COUNT(*) FROM likes l LEFT JOIN users u ON l.user_id = u.id WHERE u.id IS NULL
+UNION ALL SELECT 'likes_invalid_track', COUNT(*) FROM likes l LEFT JOIN tracks t ON l.target_id = t.id WHERE l.target_type = 'TRACK' AND t.id IS NULL
+UNION ALL SELECT 'follows_invalid_artist', COUNT(*) FROM follows f LEFT JOIN artists a ON f.artist_id = a.id WHERE a.id IS NULL
+UNION ALL SELECT 'follows_invalid_user', COUNT(*) FROM follows f LEFT JOIN users u ON f.user_id = u.id WHERE u.id IS NULL
+UNION ALL SELECT 'playbacks_invalid_playlist', COUNT(*) FROM playbacks p LEFT JOIN playlists pl ON p.playlist_id = pl.id WHERE pl.id IS NULL
+UNION ALL SELECT 'playbacks_invalid_track', COUNT(*) FROM playbacks p LEFT JOIN tracks t ON p.track_id = t.id WHERE t.id IS NULL
+UNION ALL SELECT 'playbacks_invalid_user', COUNT(*) FROM playbacks p LEFT JOIN users u ON p.user_id = u.id WHERE u.id IS NULL
+UNION ALL SELECT 'search_histories_invalid_user', COUNT(*) FROM search_histories sh LEFT JOIN users u ON sh.user_id = u.id WHERE sh.user_id IS NOT NULL AND u.id IS NULL
+UNION ALL SELECT 'playlists_invalid_user', COUNT(*) FROM playlists p LEFT JOIN users u ON p.user_id = u.id WHERE u.id IS NULL
+UNION ALL SELECT 'playlist_tracks_invalid_playlist', COUNT(*) FROM playlist_tracks pt LEFT JOIN playlists p ON pt.playlist_id = p.id WHERE p.id IS NULL
+UNION ALL SELECT 'playlist_tracks_invalid_track', COUNT(*) FROM playlist_tracks pt LEFT JOIN tracks t ON pt.track_id = t.id WHERE t.id IS NULL
+UNION ALL SELECT 'track_comments_invalid_user', COUNT(*) FROM track_comments tc LEFT JOIN users u ON tc.user_id = u.id WHERE u.id IS NULL
+UNION ALL SELECT 'track_comments_invalid_track', COUNT(*) FROM track_comments tc LEFT JOIN tracks t ON tc.track_id = t.id WHERE t.id IS NULL
+UNION ALL SELECT 'artist_applications_invalid_requester', COUNT(*) FROM artist_applications aa LEFT JOIN users u ON aa.requester_user_id = u.id WHERE u.id IS NULL
+UNION ALL SELECT 'artist_applications_invalid_reviewer', COUNT(*) FROM artist_applications aa LEFT JOIN users u ON aa.reviewed_by_admin_id = u.id WHERE aa.reviewed_by_admin_id IS NOT NULL AND u.id IS NULL
+UNION ALL SELECT 'album_applications_invalid_requester', COUNT(*) FROM album_applications aa LEFT JOIN users u ON aa.requester_user_id = u.id WHERE u.id IS NULL
+UNION ALL SELECT 'album_applications_invalid_artist', COUNT(*) FROM album_applications aa LEFT JOIN artists a ON aa.artist_id = a.id WHERE a.id IS NULL
+UNION ALL SELECT 'album_applications_invalid_reviewer', COUNT(*) FROM album_applications aa LEFT JOIN users u ON aa.reviewed_by_admin_id = u.id WHERE aa.reviewed_by_admin_id IS NOT NULL AND u.id IS NULL
+UNION ALL SELECT 'track_applications_invalid_requester', COUNT(*) FROM track_applications ta LEFT JOIN users u ON ta.requester_user_id = u.id WHERE u.id IS NULL
+UNION ALL SELECT 'track_applications_invalid_artist', COUNT(*) FROM track_applications ta LEFT JOIN artists a ON ta.artist_id = a.id WHERE ta.artist_id IS NOT NULL AND a.id IS NULL
+UNION ALL SELECT 'track_applications_invalid_album', COUNT(*) FROM track_applications ta LEFT JOIN albums a ON ta.album_id = a.id WHERE ta.album_id IS NOT NULL AND a.id IS NULL
+UNION ALL SELECT 'track_applications_invalid_reviewer', COUNT(*) FROM track_applications ta LEFT JOIN users u ON ta.reviewed_by_admin_id = u.id WHERE ta.reviewed_by_admin_id IS NOT NULL AND u.id IS NULL;
 
-
-SELECT '==================== BEHAVIOR LOG FK CHECK ====================' AS section;
-SELECT 'likes_invalid_user' AS check_name, COUNT(*) AS invalid_count
-FROM likes l
-LEFT JOIN users u ON l.user_id = u.id
-WHERE u.id IS NULL
-UNION ALL
-SELECT 'likes_invalid_track', COUNT(*)
-FROM likes l
-LEFT JOIN tracks t ON l.target_id = t.id
-WHERE l.target_type = 'TRACK' AND t.id IS NULL
-UNION ALL
-SELECT 'follows_invalid_artist', COUNT(*)
-FROM follows f
-LEFT JOIN artists a ON f.artist_id = a.id
-WHERE a.id IS NULL
-UNION ALL
-SELECT 'follows_invalid_user', COUNT(*)
-FROM follows f
-LEFT JOIN users u ON f.user_id = u.id
-WHERE u.id IS NULL
-UNION ALL
-SELECT 'playbacks_invalid_playlist', COUNT(*)
-FROM playbacks p
-LEFT JOIN playlists pl ON p.playlist_id = pl.id
-WHERE pl.id IS NULL
-UNION ALL
-SELECT 'playbacks_invalid_track', COUNT(*)
-FROM playbacks p
-LEFT JOIN tracks t ON p.track_id = t.id
-WHERE t.id IS NULL
-UNION ALL
-SELECT 'playbacks_invalid_user', COUNT(*)
-FROM playbacks p
-LEFT JOIN users u ON p.user_id = u.id
-WHERE u.id IS NULL
-UNION ALL
-SELECT 'search_histories_invalid_user', COUNT(*)
-FROM search_histories sh
-LEFT JOIN users u ON sh.user_id = u.id
-WHERE sh.user_id IS NOT NULL AND u.id IS NULL;
-
-SELECT '==================== BEHAVIOR LOG ROW COUNT ====================' AS section;
-SELECT 'likes' AS table_name, COUNT(*) AS row_count FROM likes
-UNION ALL
-SELECT 'follows', COUNT(*) FROM follows
-UNION ALL
-SELECT 'playbacks', COUNT(*) FROM playbacks
-UNION ALL
-SELECT 'search_histories', COUNT(*) FROM search_histories;
