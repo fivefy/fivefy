cdf05dd38ad02bdf0a4bd1cbc032a8b27a21f784
popo2381 <popo2381@icloud.com>
2026-05-12 19:03:53 +0900

    feat: S3 오디오 업로드 구현 추가
    - AWS SDK S3 의존성과 S3Client 설정을 추가
    - storage.audio.type=s3 조건에서 S3AudioStorageService를 활성화
    - MP3 파일 업로드 성공 시 S3 object key를 audioKey로 반환
    - local/stage/prod 오디오 저장 설정을 S3 기본 흐름에 맞게 정리

---
+++ src/main/java/com/fivefy/common/storage/S3AudioStorageConfig.java
@@ -0,0 +1,19 @@
+package com.fivefy.common.storage;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+
+@Configuration
+@ConditionalOnProperty(name = "storage.audio.type", havingValue = "s3")
+public class S3AudioStorageConfig {
+
+    @Bean
+    public S3Client s3Client(AudioStorageProperties properties) {
+        return S3Client.builder()
+                .region(Region.of(properties.region()))
+                .build();
+    }
+}
