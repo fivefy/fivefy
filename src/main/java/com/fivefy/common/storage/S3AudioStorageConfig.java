093cb019d95b1df0c4b772aab0f674d4550ff7b3
HYOKYUNG BANG <101386329+Banhklo2@users.noreply.github.com>
2026-05-13 12:34:31 +0900

    config: S3Client AWS credentials 설정 추가

--- src/main/java/com/fivefy/common/storage/S3AudioStorageConfig.java
+++ src/main/java/com/fivefy/common/storage/S3AudioStorageConfig.java
@@ -1,8 +1,11 @@
 package com.fivefy.common.storage;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 
@@ -11,9 +14,20 @@ import software.amazon.awssdk.services.s3.S3Client;
 public class S3AudioStorageConfig {
 
     @Bean
-    public S3Client s3Client(AudioStorageProperties properties) {
+    public S3Client s3Client(
+            AudioStorageProperties properties,
+            @Value("${cloud.aws.credentials.access-key}") String accessKey,
+            @Value("${cloud.aws.credentials.secret-key}") String secretKey
+    ) {
+
+        AwsBasicCredentials credentials =
+                AwsBasicCredentials.create(accessKey, secretKey);
+
         return S3Client.builder()
                 .region(Region.of(properties.region()))
+                .credentialsProvider(
+                        StaticCredentialsProvider.create(credentials)
+                )
                 .build();
     }
 }
