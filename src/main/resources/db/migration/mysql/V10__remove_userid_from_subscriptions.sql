8189a073cc8802b35c9d0ad6a134868d0d0b422a
곽현민 <hunmin111@gmail.com>
2026-05-14 10:56:01 +0900

    docs: 프로젝트 과정 및 설명을 위한 README 수정
    - 프로젝트 과정 및 설명을 위한 README 내용 업데이트
    - MySQL 마이그레이션 v1~v11 내용 히스토리 통합

--- src/main/resources/db/migration/mysql/V10__remove_userid_from_subscriptions.sql
+++
@@ -1,3 +0,0 @@
--- subscriptions 테이블에서 user_id 컬럼 제거
--- 롤백
-ALTER TABLE subscriptions DROP COLUMN user_id;
--- src/main/resources/db/migration/mysql/V3__add_performance_indexes.sql
+++
@@ -1,9 +0,0 @@
-ALTER TABLE tracks
-    ADD INDEX idx_track_status_deleted_published (status, deleted_at, published_at DESC),
-    ALGORITHM=INPLACE,
-    LOCK=NONE;
-
-ALTER TABLE track_comments
-    ADD INDEX idx_track_comment_track_deleted_created (track_id, deleted_at, created_at DESC),
-    ALGORITHM=INPLACE,
-    LOCK=NONE;
